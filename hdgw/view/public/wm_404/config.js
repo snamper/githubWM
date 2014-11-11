5029ff5cdbfddd87b6e35f04b176c7e1749a8231
chaiyining007 <130016001@qq.com>
2014-11-11 21:39:08 +0800

    公共模块

---
+++ hdgw/view/public/wm_404/config.js
@@ -0,0 +1,44 @@
+﻿seajs.config({
+    'base': '/',
+    'alias': {
+        'domains': 'http://a.myhd.wumeiwang.com/hdgw/model/domains/dist/domains.js',
+        'public': 'http://a.myhd.wumeiwang.com/hdgw/view/public/dist/public.js',
+        'agoPublic': 'http://a.myhd.wumeiwang.com/hdgw/view/ago/public/dist/public.js',
+        'jquery': 'http://a.myhd.wumeiwang.com/hdgw/model/lib/jquery/seajs-jquery-1.8.3.min.js',
+        'lib': 'http://a.myhd.wumeiwang.com/hdgw/model/lib/dist/lib.js',
+        'wmtips': 'http://a.myhd.wumeiwang.com/hdgw/model/wmtips/dist/wmtips.js',
+        'wmforimg': 'http://a.myhd.wumeiwang.com/hdgw/model/wmforimg/dist/forimg.js',
+        'wmlazyload': 'http://a.myhd.wumeiwang.com/hdgw/model/wmlazyload/dist/wmlazyload.js',
+        'loginBox': 'http://a.myhd.wumeiwang.com/hdgw/model/wmloginbox/dist/wmloginbox.js'
+    },
+    'map': [
+       [/^(.*\.(?:css|js))(.*)$/i, '$1?t=2013122701']
+    ]
+});
+(function () {
+    var dev = false, //上线时，修改为false
+       scripts = document.scripts,
+       script = scripts[scripts.length - 1],
+       boot = script.getAttribute('data-init'),
+       dir = script.getAttribute('src');
+
+    dir = dir.slice(0, dir.lastIndexOf('/') + 1);
+    //dev 
+    if (dev) {
+        dir = dir + 'src/';
+    } else {
+        dir = dir + 'dist/';
+    }
+    /*
+     * 上面获取路径脚本需要立刻执行
+     * 将加载脚本放到domReady后执行，避免ie浏览器终止操作错误
+     */
+    seajs.use('jquery', function ($) {
+        $(function () {
+            seajs.use('public');
+            if (boot) {
+                seajs.use(dir + boot);
+            }
+        });
+    });
+})();
---
+++ hdgw/view/public/wm_out_share/src/out_share.js
@@ -0,0 +1,50 @@
+﻿define(function (require, exports, module) {"require:nomunge,exports:nomunge,module:nomunge"; var domains = require('domains');
+    var $ = require('http://a.myhd.wumeiwang.com/hdgw/model/lib/jquery/seajs-jquery-1.8.3.min.js'),
+        juicer = require('http://a.myhd.wumeiwang.com/hdgw/model/lib/juicer/seajs-juicer-min.js');
+    var shareOpenUrl = {
+        "tx": function (title, url, pic) {
+            return "http://share.v.t.qq.com/index.php?c=share&a=index&site=www.hdgw.com&assname=wumei2013&" + $.param({
+                title: title,
+                url: encodeURIComponent(url),
+                pic: pic
+            });
+        },
+        "sina": function (title, url, pic) {
+            return "http://service.weibo.com/share/share.php?content=utf8&" + $.param({
+                sourceUrl: encodeURIComponent(domains.www),
+                source: encodeURIComponent("互动购物"),
+                title: encodeURIComponent(title),
+                url: encodeURIComponent(url),
+                pic: pic
+            });
+        },
+        "renren": function (url) {
+            return "http://share.renren.com/share/buttonshare.do?" + $.param({
+                link: encodeURIComponent(url)
+            });
+        },
+        "qzone": function (title, url, pic) {
+            return "http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?" + $.param({
+                summary: encodeURIComponent(title),
+                title: title,
+                url: encodeURIComponent(url),
+                site: encodeURIComponent("互动购物"),
+                pics: pic
+            });
+        }
+    }
+    var _init = function (ele) {
+        var $ele = $(ele);
+        $ele.each(function () {
+            var $this = $(this);
+            var type = $this.attr("data_type"),
+                title = $this.attr("data_title"),
+                pic = $this.attr("data_pic"),
+                url = $this.attr("data_url") || window.location.href;
+            $this.attr("href", shareOpenUrl[type](title, url, pic));
+        });
+    }
+    exports.init = function (ele) {
+        _init(ele);
+    }
+});
