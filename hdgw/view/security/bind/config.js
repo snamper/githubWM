d529b7cb8f3ee04b3c07d13139db9af8b003c779
chaiyining007 <130016001@qq.com>
2014-11-11 21:37:08 +0800

    安全模块

---
+++ hdgw/view/security/bind/config.js
@@ -0,0 +1,45 @@
+﻿seajs.config({
+    'base': '/',
+    'alias': {
+        'domains': 'http://a.myhd.wumeiwang.com/hdgw/model/domains/dist/domains.js',
+        'public': 'http://a.myhd.wumeiwang.com/hdgw/view/public/dist/public.js',
+        'buyersPublic': 'http://a.myhd.wumeiwang.com/hdgw/view/buyers/public/dist/public.js',
+        'jquery': 'http://a.myhd.wumeiwang.com/hdgw/model/lib/jquery/seajs-jquery-1.8.3.min.js',
+        'lib': 'http://a.myhd.wumeiwang.com/hdgw/model/lib/dist/lib.js',
+        'wmverification': 'http://a.myhd.wumeiwang.com/hdgw/model/wmverification/dist/wmverification.js',
+        'wmtips': 'http://a.myhd.wumeiwang.com/hdgw/model/wmtips/dist/wmtips.js',
+        'wmbox': 'http://a.myhd.wumeiwang.com/hdgw/model/wmbox/dist/wmbox.js',
+        'juicer': 'http://a.myhd.wumeiwang.com/hdgw/model/lib/juicer/seajs-juicer-min.js'
+    },
+    'map': [
+       [/^(.*\.(?:css|js))(.*)$/i, '$1?t=2014020801']
+    ]
+});
+(function () {
+    var dev = false, //上线时，修改为false
+        scripts = document.scripts,
+        script = scripts[scripts.length - 1],
+        boot = script.getAttribute('data-init'),
+        dir = script.getAttribute('src');
+
+    dir = dir.slice(0, dir.lastIndexOf('/') + 1);
+    //dev 
+    if (dev) {
+        dir = dir + 'src/';
+    } else {
+        dir = dir + 'dist/';
+    }
+    /*
+     * 上面获取路径脚本需要立刻执行
+     * 将加载脚本放到domReady后执行，避免ie浏览器终止操作错误
+     */
+    seajs.use('jquery', function ($) {
+        $(function () {
+            seajs.use('public');
+            seajs.use('buyersPublic');
+            if (boot) {
+                seajs.use(dir + boot);
+            }
+        });
+    });
+})();
